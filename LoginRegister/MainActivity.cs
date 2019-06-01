be6d1d3a270e1e1995d6371e412320ad0c950d71
RahulGajare <rahulgajare0>
2019-06-01 19:17:22 +0530

    Login Register

---
+++ LoginRegister/MainActivity.cs
@@ -0,0 +1,52 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System;
+using Android.Content;
+using Android.Gms.Auth.Api.SignIn;
+using Android.Gms.Common.Apis;
+using Android.Gms.Auth.Api;
+using Android.Gms.Common;
+using Android.Content.PM;
+using Java.Security;
+
+namespace LoginRegister
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+       
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            PackageInfo info = this.PackageManager.GetPackageInfo("LoginRegister.LoginRegister", PackageInfoFlags.Signatures );
+
+            foreach (Android.Content.PM.Signature signature in info.Signatures)
+            {
+                MessageDigest md = MessageDigest.GetInstance("SHA");
+                md.Update(signature.ToByteArray());
+
+                string keyhash = Convert.ToBase64String(md.Digest());
+                Console.WriteLine("etwwt",keyhash);
+            }
+
+            var button = FindViewById<Button>(Resource.Id.Register_clicked);
+            button.Click += OnRegister_Clicked;
+            var socialButton = FindViewById<Button>(Resource.Id.button2);                   
+        }
+       
+        private void OnRegister_Clicked(object sender, EventArgs e)
+        {
+            var intent = new Intent(this, typeof(Register));
+            StartActivity(intent);
+        }
+
+      
+
+    }
+}
---
+++ LoginRegister/Register.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Views;
+using Android.Widget;
+using Firebase.Auth;
+
+namespace LoginRegister
+{
+    [Activity(Label = "Register")]
+    public class Register : Activity
+    {
+        LinearLayout snbar;
+        string email;
+        string password;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.RegisterPage);
+           var registerButton = FindViewById<Button>(Resource.Id.Register2);
+            registerButton.Click += registerButton_ClickedAsync;
+           
+
+        }
+
+        private async void registerButton_ClickedAsync(object sender, EventArgs e)
+        {
+            var email = FindViewById<EditText>(Resource.Id.Email);
+            this.email = email.Text;
+            var password = FindViewById<EditText>(Resource.Id.Password);
+            this.password = password.Text;
+            var response = await FirebaseAuth.Instance.CreateUserWithEmailAndPasswordAsync(this.email, this.password);
+
+            Snackbar snackbar = Snackbar.Make(snbar, "Registered Successfully ", Snackbar.LengthShort);
+                snackbar.Show();
+            
+        }
+    }
+}
